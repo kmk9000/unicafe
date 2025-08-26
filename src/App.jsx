73bd79403a2eefa09d24ddb5bc7bea1b6daa06e3
kmk9000 <kallekoivuniemi@gmail.com>
2025-08-26 13:57:04 +0300

    refactor statistics component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from "react";
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statistics = (props) => {
+  const { good, neutral, bad } = props;
+  console.log(props);
 
   const total = good + neutral + bad;
+  console.log(total);
 
   const average = (good * 1 + neutral * 0 + bad * -1) / total;
+  console.log(average);
   const positive = (good / total) * 100;
+  console.log(positive);
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+    <>
       <h1>statistics</h1>
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
@@ -24,6 +21,23 @@ const App = () => {
       <p>total: {total}</p>
       <p>average: {average}</p>
       <p>positive: {positive} %</p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
