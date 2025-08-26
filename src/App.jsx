ed565a90cdb1b4648c660a460bc6440150b53516
kmk9000 <kallekoivuniemi@gmail.com>
2025-08-26 14:33:22 +0300

    display statistics only once feedback has been gathered

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const Statistics = (props) => {
 
   const average = (good * 1 + neutral * 0 + bad * -1) / total;
   console.log(average);
+
   const positive = (good / total) * 100;
   console.log(positive);
 
@@ -31,13 +32,19 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const total = good + neutral + bad;
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
   );
 };
